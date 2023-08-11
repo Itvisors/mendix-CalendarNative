33903bb2e5fc8b17c5da29cf75c1fb27a07ca355
Mats Cornelissen <matscornelissen@hotmail.com>
2023-08-11 15:01:50 +0200

    multi dot period finished

--- src/mappings/multiPeriodMarking.js
+++ src/mappings/multiPeriodMarking.js
@@ -1,10 +1,15 @@
 import { CalendarUtils } from "react-native-calendars";
 
+const addDaysToDate = (date, days) => {
+    let newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + days);
+    return CalendarUtils.getCalendarDateString(newDate);
+  };
 
 function differenceInDays(date1, date2) {
     const difference = date1.getTime() - date2.getTime();
     const TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    return TotalDays;
+    return Math.abs(TotalDays);
 }
 
 export function multiPeriodMarking(events, eventStartDate, eventEndDate, eventDotColor, selectedDay) {
@@ -25,13 +30,19 @@ export function multiPeriodMarking(events, eventStartDate, eventEndDate, eventDo
                     periods: [period]
                 };
             }
-        // } else {
-            
-        //     daysInBetween = differenceInDays(eventStartDate.get(event).value, eventEndDate.get(event).value);
-        //     i = 0;
-        //     while (i < daysInBetween) {
-        //         ii++
-        //     }
+        } else {
+            daysInBetween = differenceInDays(eventStartDate.get(event).value, eventEndDate.get(event).value);
+            for (let i = 0; i < daysInBetween + 1; i++) {
+                const dateString = addDaysToDate(eventStartDate.get(event).value,i);
+                period = {startingDay: i === 0 ? true : false, endingDay: i === daysInBetween ? true : false, color: color}
+                if (periodArray[dateString]) {
+                    periodArray[dateString].periods.push(period);
+                } else {
+                    periodArray[dateString] = {
+                        periods: [period]
+                    };
+                }
+            }
         }
     });
 
