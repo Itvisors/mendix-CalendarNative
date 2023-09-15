dcc3d37148778f019ab83bebf4598cdcb08ff074
Johan Munneke <johanmunneke94@gmail.com>
2023-09-15 14:44:36 +0200

    Translated today

--- src/utils/setLocaleConfig.js
+++ src/utils/setLocaleConfig.js
@@ -1,14 +1,16 @@
 import { LocaleConfig } from 'react-native-calendars';
+import { todayTranslations } from './todayTranslations'
 import moment from 'moment/min/moment-with-locales';
 
 export function setLocaleConfig(locale) {
   moment.locale(locale);
-    LocaleConfig.locales[locale] = {
-      monthNames: moment.months(),
-      monthNamesShort: moment.monthsShort(),
-      dayNames: moment.weekdays(),
-      dayNamesShort: moment.weekdaysShort(),
-      today: "Today"
-    };
-    LocaleConfig.defaultLocale = locale;
+  const today = todayTranslations[locale] ? todayTranslations[locale] : 'Today';
+  LocaleConfig.locales[locale] = {
+    monthNames: moment.months(),
+    monthNamesShort: moment.monthsShort(),
+    dayNames: moment.weekdays(),
+    dayNamesShort: moment.weekdaysShort(),
+    today: today
+  };
+  LocaleConfig.defaultLocale = locale;
 }
